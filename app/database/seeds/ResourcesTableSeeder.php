6744e63b1dcc3974fcdadd490e9d4e6559701127
Roberto <roberto.butti@gmail.com>
2013-12-26 16:03:29 +0100

    seeding

--- app/database/seeds/ResourcesTableSeeder.php
+++ app/database/seeds/ResourcesTableSeeder.php
@@ -5,14 +5,28 @@ class ResourcesTableSeeder extends Seeder {
 	public function run()
 	{
 		// Uncomment the below to wipe the table clean before populating
-		// DB::table('resources')->truncate();
-
-		$resources = array(
-
-		);
+		DB::table('resources')->truncate();
+    $resources = array();
+    $file_array = __DIR__."/my_array_data.php";
+    if (file_exists($file_array)) {
+      require($file_array);
+    }
+    
+    foreach ($resources as $key => $value) {
+      $resource = Resource::create(
+        array(
+          'first_name' => $value[0],
+          'last_name' => $value[1],
+          'email' => $value[2],
+          "profile" => $value[3],
+          "contract" => $value[4],
+          "active" => true
+        )
+      );
+    }
 
 		// Uncomment the below to run the seeder
-		// DB::table('resources')->insert($resources);
+		//DB::table('resources')->insert($resources);
 	}
 
 }
